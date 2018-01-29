ff1be0dcdc2849f2bc8e9bef9218e01c1554fd50
weisico <mt3530@163.com>
2018-01-29 18:10:40 +0800

    20180129

--- app/src/main/java/com/weisico/sqljdbc/DBUtil.java
+++ app/src/main/java/com/weisico/sqljdbc/DBUtil.java
@@ -1,13 +1,22 @@
 package com.weisico.sqljdbc;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DBUtil  {
 
@@ -108,17 +117,10 @@ public class DBUtil  {
             String sql="kill "+blk+" ";
             Statement stmt = conn.createStatement();//
             stmt.executeUpdate(sql);
-//            while (rs.next())
-//            {
-//                String s1 = rs.getString(0);
-//
-//                result += s1 +"-"+"\n";
-//                System.out.println(s1+"+" );
-//            }
             System.out.println("解锁成功" );
-            result="解锁成功";
             stmt.close();
             conn.close();
+            result="解锁成功";
         } catch (SQLException e)
         {
             e.printStackTrace();
@@ -126,9 +128,50 @@ public class DBUtil  {
         }
         return result;
     }
+    public static ArrayList  show() {
+        ArrayList<Map<String, Object>> listItems = null;
+        try {
+            Connection conn = getSQLConnection("172.17.182.100", "ytsoft", "146-164-154-", "bqdata_new");
+            //String sql = "select top 10 * from bq_czy";
+            String sql = " select top 10 b.锁进程ID,b.锁进程用户主机" +
+                    " from(select   spid 被锁进程ID,blocked 锁进程ID, status 被锁状态,"+
+                    " SUBSTRING(hostname,1,12) 被锁进程用户主机, SUBSTRING(DB_NAME(dbid),1,10) 被锁进程数据名称, cmd 被锁进程命令"+
+                    " FROM master..sysprocesses" +
+                    " WHERE blocked>0 ) as a" +
+                    " left join (select isnull(spid,0) 锁进程ID,status 锁状态, cmd 锁进程命令,SUBSTRING(hostname,1,12) 锁进程用户主机, " +
+                    " net_address MAC地址,SUBSTRING(DB_NAME(dbid),1,10) 锁进程数据名称" +
+                    " from master..sysprocesses " +
+                    " ) as b on a.锁进程ID=b.锁进程ID " +
+                    " left join (select ksmc,replace([mac_address],'-','')mac_address  " +
+                    " from openrowset('SQLOLEDB ', '172.17.180.200'; 'psa'; '146-164-154-',[Blog_new].[dbo].[Mac_new])) as c  " +
+                    " on b.MAC地址=c.mac_address";
+            Statement stmt = conn.createStatement();//
+            ResultSet rs = stmt.executeQuery(sql);
+            listItems = new ArrayList<Map<String, Object>>();
+            Map<String, Object> listItem;
+            while (rs.next()) {
+                listItem = new HashMap<String, Object>();
+                listItem.put("tv1", rs.getString("锁进程ID"));
+                listItem.put("tv2", rs.getString("锁进程用户主机"));
+                listItems.add(listItem);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage().toString());
+            //            result += "查询数据异常!" + e.getMessage();
+        }
+        return listItems;
+
+    }
 
     public static void main(String[] args)
     {
         QuerySQL();
     }
+
 }
--- app/src/main/java/com/weisico/sqljdbc/MainActivity.java
+++ app/src/main/java/com/weisico/sqljdbc/MainActivity.java
@@ -8,7 +8,12 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +22,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private View btnTest;
@@ -25,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private View btnGet;
     private EditText tvCmd;
     private View btnUnlock;
-
+    private ListView sqlList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,15 +43,18 @@ public class MainActivity extends AppCompatActivity {
         btnClean = findViewById(R.id.btnClean);
         tvTestResult = (TextView) findViewById(R.id.tvTestResult);
         btnUnlock=findViewById(R.id.btnUnlock);
-
+        sqlList=(ListView)findViewById(R.id.list_item);
         tvTestResult.setMovementMethod(new ScrollingMovementMethod());
 
         btnGet=findViewById(R.id.btnGet);
         tvCmd=(EditText)findViewById(R.id.tvCmd);
 
+        //刷新按钮监听命令
         btnTest.setOnClickListener(getClickEvent());
+        //清除按钮监听命令
         btnClean.setOnClickListener(getClickEvent());
         //btnGet.setOnClickListener(getCmd());
+        //查看按钮监听命令
         btnGet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        //解锁按钮监听命令
         btnUnlock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,22 +86,40 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        //获取点击item信息
+        sqlList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                HashMap<String, String> map = (HashMap<String, String>) parent
+                        .getItemAtPosition(position);
+//                Toast.makeText(view.getContext(), map.get("tv1"),
+//                        Toast.LENGTH_SHORT).show();
+                if(sqlList.getCount()>0) {
+                    tvCmd.setText(map.get("tv1"));
+                }
+            }
+        });
+
+        //
     }
-
+    //刷新按钮 查询进程
     private View.OnClickListener getClickEvent() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tvTestResult.setText("...");
                 if (v == btnTest) {
-                    test();
+                   // test();
+                    list();
 
                 }
             }
         };
     }
-    //查看 进程
+    //查看 进程命令
     private View.OnClickListener getCmd()
     {
 //        String num = tvCmd.getText().toString();
@@ -108,6 +139,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
+    //解锁进程
     private View.OnClickListener ToUnlock()
     {
         String s=tvCmd.getText().toString();
@@ -127,8 +159,10 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
+    // 查看进程命令 方法
     private void DEMO() {
         final Integer blk;
+        sqlList.setVisibility(View.GONE);
         blk =Integer.parseInt(tvCmd.getText().toString());
         Runnable run = new Runnable() {
             @Override
@@ -145,8 +179,9 @@ public class MainActivity extends AppCompatActivity {
         new Thread(run).start();
 
     }
+    //解锁进程方法
     private void Unlock() {
-
+        sqlList.setVisibility(View.VISIBLE);
         final Integer blk;
         blk =Integer.parseInt(tvCmd.getText().toString());
         Runnable run = new Runnable() {
@@ -164,7 +199,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(run).start();
 
     }
-
+    //刷新获取进程方法
     private void test() {
         Runnable run = new Runnable() {
             @Override
@@ -181,7 +216,72 @@ public class MainActivity extends AppCompatActivity {
         new Thread(run).start();
 
     }
+    //参数形式 获取listview 信息··获取数据库数据绑定到listview
+    private void list() {
+        sqlList.setVisibility(View.VISIBLE);
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
 
+                ArrayList<Map<String, Object>> ret = DBUtil.show();
+                Message message = new Message();
+                message.what = 1001;
+                message.obj = ret;
+                handler2.sendMessage(message);
+            }
+        };
+//        sqlList.getChildAt(0);
+//        if(sqlList.getCount()>0) {
+//            //            tvCmd.setText(sqlList.);
+//            HashMap<String, String> map = (HashMap<String, String>) sqlList
+//                    .getItemAtPosition(0);
+//            tvCmd.setText(map.get("tv1"));
+//
+//        }
+        new Thread(run).start();
+
+
+    }
+    //接收从子线程发回来的结果进行处理
+    Handler  handler2= new Handler(){
+        public void handleMessage(android.os.Message msg) {
+            if(msg.what == 1001){
+                List<Map<String, Object>> data = (List<Map<String, Object>>) msg.obj;
+                SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,data,
+                        R.layout.moban,
+                        new String[]{"tv1","tv2"},
+                        new int[]{R.id.tv1,R.id.tv2});
+                sqlList.setAdapter(adapter);
+//               setListViewHeightBasedOnChildren(sqlList);
+            }
+        }
+    };
+    /**
+     * 为了解决ListView在ScrollView中只能显示一行数据的问题
+     *
+     * @param listView
+     */
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        // 获取ListView对应的Adapter
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0, len = listAdapter.getCount(); i < len; i++) { // listAdapter.getCount()返回数据项的数目
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0); // 计算子项View 的宽高
+            totalHeight += listItem.getMeasuredHeight(); // 统计所有子项的总高度
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight
+                + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        // listView.getDividerHeight()获取子项间分隔符占用的高度
+        // params.height最后得到整个ListView完整显示需要的高度
+        listView.setLayoutParams(params);
+    }
     Handler mHandler = new Handler() {
         public void handleMessage(android.os.Message msg) {
             switch (msg.what) {
